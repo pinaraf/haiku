cc3cd7c2d42c51e4a1c4de20fb645b2329109586
Pierre Ducroquet <pinaraf@pinaraf.info>
2025-05-05 18:28:18 +0200

    progress
    Change-Id: Ibde77dd8396a62d60a325b15f04d897756bc0d25

--- src/add-ons/print/drivers/pwg/pwg.cpp
+++ src/add-ons/print/drivers/pwg/pwg.cpp
@@ -7,6 +7,8 @@
 #include "pwg.h"
 
 #include "DbgMsg.h"
+#include <inttypes.h>
+
 #if 0
 #include <vector>
 #include <stdio.h>
@@ -27,8 +29,8 @@
 #include "PSCap.h"
 #include "PSData.h"
 #include "UIDriver.h"
-#include "ValidRect.h"
 #endif
+#include "ValidRect.h"
 
 #define dbg_msg(x) WriteSpoolData((x), strlen((x)))
 
@@ -44,7 +46,7 @@ bool
 PWGDriver::StartDocument()
 {
 	DBGMSG(("> startDocument\n"));
-	dbg_msg("> startDocument\n");
+	WriteSpoolData("RaS2", 4);
 	return true;
 }
 
@@ -52,8 +54,12 @@ PWGDriver::StartDocument()
 bool
 PWGDriver::StartPage(int page)
 {
+	// TODO: map from job data to the header
+	PWGPageHeader header;
+	memset(&header, 97, sizeof(PWGPageHeader)); // 'a' everywhere
+	// memset(&header, 0, sizeof(PWGPageHeader));
 	DBGMSG(("> startPage\n"));
-	dbg_msg("> startPage\n");
+	WriteSpoolData(&header, sizeof(PWGPageHeader));
 	return true;
 }
 
@@ -62,7 +68,6 @@ bool
 PWGDriver::EndPage(int)
 {
 	DBGMSG(("> endPage\n"));
-	dbg_msg("> endPage\n");
 	return true;
 }
 
@@ -71,7 +76,6 @@ bool
 PWGDriver::EndDocument(bool)
 {
 	DBGMSG(("> endDocument\n"));
-	dbg_msg("> endDocument\n");
 	return true;
 }
 
@@ -80,11 +84,158 @@ bool
 PWGDriver::NextBand(BBitmap* bitmap, BPoint* offset)
 {
 	DBGMSG(("> nextBand\n"));
-	dbg_msg("> nextBand\n");
+
+	BRect bounds = bitmap->Bounds();
+
+	DBGMSG((" this band has %d bytes\n", bitmap->BitsLength()));
+
+	RECT rc;
+	rc.left = (int)bounds.left;
+	rc.top = (int)bounds.top;
+	rc.right = (int)bounds.right;
+	rc.bottom = (int)bounds.bottom;
+
+	int height = rc.bottom - rc.top + 1;
+
+	int x = (int)offset->x;
+	int y = (int)offset->y;
+
+	int page_height = GetPageHeight();
+
+	if (y + height > page_height) {
+		height = page_height - y;
+	}
+
+	rc.bottom = height - 1;
+
+	if (get_valid_rect(bitmap, &rc)) {
+		DBGMSG(("validate rect = %d, %d, %d, %d\n",
+                                rc.left, rc.top, rc.right, rc.bottom));
+		x = rc.left;
+		y += rc.top;
+		
+		int width = rc.right - rc.left + 1;
+		int height = rc.bottom - rc.top + 1;
+		
+		DBGMSG(("x = %d\n", x));
+		DBGMSG(("y = %d\n", y));
+		DBGMSG(("width = %d\n", width));
+		DBGMSG(("height = %d\n", height));
+		
+		DBGMSG(("color_space = %d\n", bitmap->ColorSpace()));
+		DBGMSG(("bpr = %d\n", bitmap->BytesPerRow()));
+		
+		
+		if (bitmap->ColorSpace() == B_RGB32) {
+			DBGMSG(("B_RGB32, let's go\n"));
+			// Skip lines before rc.top
+			
+			uint32_t *bitmapData = (uint32_t *) bitmap->Bits();
+			// Handle valid lines
+			int pixelsPerLine = bounds.right - bounds.left + 1;
+			int validBytesPerRow = (rc.right - rc.left) * 4;
+			DBGMSG(("validBytesPerRow = %d\n", validBytesPerRow));
+			uint32_t *lineData = new uint32_t[validBytesPerRow / 4];
+			memset(lineData, 0, validBytesPerRow);
+			int lineRepeatCount = -1;
+			
+			for (int line = rc.top ; line < rc.bottom ; line++) {
+				//DBGMSG(("Checking line %d\n", line));
+				if ((memcmp(lineData, &(bitmapData[line*pixelsPerLine + rc.left]), validBytesPerRow) == 0) && (lineRepeatCount < 255))
+				{
+					lineRepeatCount++;
+				} else {
+					// flush previous line if needed
+					if (lineRepeatCount > -1) {
+						dumpLine(lineRepeatCount, lineData, validBytesPerRow / 4);
+					}
+					// Store new line info
+					memcpy(lineData, &(bitmapData[line*pixelsPerLine + rc.left]), validBytesPerRow);
+					lineRepeatCount = 0;
+				}
+			}
+			if (lineRepeatCount > -1) {
+				// flush final lines
+				dumpLine(lineRepeatCount, lineData, validBytesPerRow / 4);
+			}
+			
+			delete[] lineData;
+			
+			// Skip lines after rc.bottom
+		}
+
+	} else {
+		DBGMSG(("Could not get a valid rect, weird\n"));
+	}
+
 	
 	offset->x = -1.0;
 	offset->y = -1.0;
+
+
 	return true;
 }
 
+void PWGDriver::dumpLine(int repeatCount, uint32_t *lineData, int length)
+{
+	uint8_t truncatedRepeatCount = repeatCount;
+	WriteSpoolData(&truncatedRepeatCount, 1);
 
+	DBGMSG(("Repeat %d:", repeatCount));
+	uint32_t currentByte = lineData[0];
+	int repeat = 0;
+	for (int col = 1 ; col < length ; col++) {
+		if ((currentByte == lineData[col]) && (repeat < 255)) {
+			repeat++;
+		} else {
+			truncatedRepeatCount = repeat;
+			WriteSpoolData(&truncatedRepeatCount, 1);
+			WriteSpoolData(((char*)&currentByte)+1, 3);
+			
+			DBGMSG(("%d * %x ; " B_PRIu32, repeat, currentByte));
+			currentByte = lineData[col];
+			repeat = 0;
+		}
+		//DBGMSG(("%x" B_PRIu32, lineData[col]));
+	}
+	truncatedRepeatCount = repeat;
+	WriteSpoolData(&truncatedRepeatCount, 1);
+	WriteSpoolData(((char*)&currentByte)+1, 3);
+	DBGMSG(("%d * %x" B_PRIu32, repeat, currentByte));
+	DBGMSG(("\n"));
+}
+
+
+/*
+****************
+page_width  = 2363
+page_height = 3388
+band_count  = 1
+band_height = 3388
+****************
+nup        = 1
+page_count = 1
+reverse    = false
+> startDocument
+page index = 0
+> startPage
+picture_count = 1
+construct PictureData
+1: current seek position = 0x651
+picture_data::point = 0.000000, 0.000000
+picture_data::rect = 14.400001, 14.400001, 0.000000, 3.000000
+2: current seek position = 0x669
+3: current seek position = 0x82d
+> nextBand
+validate rect = 17, 20, 131, 58
+x = 17
+y = 20
+width = 115
+height = 39
+> endPage
+> endDocument
+	************ START - DUMP BMessage ***********
+	address: 0xc916ad9e00
+	msg->what: okok
+	************ END - DUMP BMessage ***********
+ */
--- src/add-ons/print/drivers/pwg/pwg.h
+++ src/add-ons/print/drivers/pwg/pwg.h
@@ -9,6 +9,111 @@
 #include "GraphicsDriver.h"
 
 
+enum WhenEnum : uint32_t {
+	Never = 0,
+	AfterDocument = 1,
+	AfterJob = 2,
+	AfterSet = 3,
+	AfterPage = 4
+};
+
+using Boolean = uint32_t;
+
+struct Dimension {
+	uint32_t x;
+	uint32_t y;
+};
+
+enum EdgeEnum : uint32_t {
+	ShortEdgeFirst = 0,
+	LongEdgeFirst = 1
+};
+
+enum MediaPositionEnum : uint32_t {
+};
+
+enum OrientationEnum : uint32_t {
+	Portrait = 0,
+	Landscape = 1,
+	ReversePortrait = 2,
+	ReverseLandscape = 3
+};
+
+enum ColorOrderEnum : uint32_t {
+	Chunky = 0
+};
+
+enum ColorSpaceEnum : uint32_t {
+};
+
+// What is this one?
+using SrgbColor = uint32_t;
+
+enum PrintQualityEnum : uint32_t {
+	Default = 0,
+	Draft = 3,
+	Normal = 4,
+	High = 5
+};
+
+struct PWGPageHeader {
+	char PwgRaster[64];
+	char MediaColor[64];
+	char MediaType[64];
+	char PrintContentOptimize[64];
+	char padding1[12];
+	WhenEnum CutMedia;
+	Boolean Duplex;
+	Dimension HWResolution;
+	char padding2[16];
+	Boolean InsertSheet;
+	WhenEnum Jog;
+	EdgeEnum LeadingEdge;
+	char padding3[12];
+	MediaPositionEnum MediaPosition;	
+	uint32_t MediaWeightMetric;
+	char padding4[8];
+	uint32_t NumCopies;
+	OrientationEnum Orientation;
+	char padding5[4];
+	Dimension PageSize;
+	char padding6[8];
+	Boolean Tumble;
+	uint32_t Width;
+	uint32_t Height;
+	char padding7[4];
+	uint32_t BitsPerColor;
+	uint32_t BitsPerPixel;
+	uint32_t BytesPerLine;
+	ColorOrderEnum ColorOrder;
+	ColorSpaceEnum ColorSpace;
+	char padding8[16];
+	uint32_t NumColors;
+	char padding9[28];
+	uint32_t TotalPageCount;
+	int32_t CrossFeedTransform;
+	int32_t FeedTransform;
+	uint32_t ImageBoxLeft;
+	uint32_t ImageBoxTop;
+	uint32_t ImageBoxRight;
+	uint32_t ImageBoxBottom;
+	SrgbColor AlternatePrimary;
+	PrintQualityEnum PrintQuality;
+	char padding10[20];
+	uint32_t VendorIdentifier;
+	uint32_t VendorLength;
+	char VendorData[1088];
+	char padding11[64];
+	char RenderingIntent[64];
+	char PageSizeName[64];
+};
+
+static_assert(offsetof(PWGPageHeader, InsertSheet) == 300, "Invalid offset of field InsertSheet");
+static_assert(offsetof(PWGPageHeader, ColorSpace) == 400, "Invalid offset of field ColorSpace");
+static_assert(offsetof(PWGPageHeader, TotalPageCount) == 452, "Invalid offset of field TotalPageCount");
+static_assert(offsetof(PWGPageHeader, AlternatePrimary) == 480, "Invalid offset of field AlternatePrimary");
+static_assert(offsetof(PWGPageHeader, VendorData) == 516, "Invalid offset of field VendorData");
+static_assert(sizeof(PWGPageHeader) == 1796, "Invalid size for the PWGPageHeader structure");
 
 class PWGDriver : public GraphicsDriver {
 public:
@@ -23,6 +128,7 @@ protected:
 	virtual	bool		EndDocument(bool success);
 
 private:
+	void dumpLine(int repeatCount, uint32_t *lineData, int length);
 };
 
 #endif // __PWG_H
